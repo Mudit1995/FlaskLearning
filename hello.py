cd7362e210d87bcdfc56a3a74fd8d0023d4f10c7
Mudit Mohan Aggarwal <Mudit.m.aggarwal@gmail.com>
2024-03-08 03:04:14 +0530

    tweaked 500 page

--- hello.py
+++ hello.py
@@ -49,7 +49,7 @@ def user(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-# Internal Server Error
+# Internal Server Error thing
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
